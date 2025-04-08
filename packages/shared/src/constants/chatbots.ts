993beb02f85becff28fdd42e7d48a16488b507fc
Robert Piosik <robertpiosik@gmail.com>
2025-04-08 14:49:46 +0200

    Reverse the model key-value pairs to use model IDs as keys and display names as values, and add `supported_options` for Gemini and DeepSeek

--- packages/shared/src/constants/chatbots.ts
+++ packages/shared/src/constants/chatbots.ts
@@ -5,8 +5,11 @@ type Chatbot = {
     supports_system_instructions?: boolean
     supports_user_provided_model?: boolean
     supports_user_provided_port?: boolean
+    supported_options?: {
+      [option: string]: string
+    }
     models?: {
-      [display_name: string]: string
+      [model: string]: string
     }
   }
 }
@@ -17,19 +20,20 @@ export const CHATBOTS: Chatbot = {
     supports_custom_temperature: true,
     supports_system_instructions: true,
     models: {
-      'Gemini 2.0 Flash': 'gemini-2.0-flash',
-      'Gemini 2.0 Flash-Lite': 'gemini-2.0-flash-lite',
-      'Gemini 2.5 Pro Preview 03-25': 'gemini-2.5-pro-preview-03-25',
-      'Gemini 2.0 Flash Thinking Exp 01-21':
-        'gemini-2.0-flash-thinking-exp-01-21'
+      'gemini-2.0-flash': 'Gemini 2.0 Flash',
+      'gemini-2.0-flash-lite': 'Gemini 2.0 Flash-Lite',
+      'gemini-2.5-pro-preview-03-25': 'Gemini 2.5 Pro Preview 03-25',
+      'gemini-2.0-flash-thinking-exp-01-21':
+        'Gemini 2.0 Flash Thinking Exp 01-21'
     }
   },
   Gemini: {
     url: 'https://gemini.google.com/app',
+    supported_options: { canvas: 'Canvas' },
     models: {
-      '2.0 Flash': '2.0-flash',
-      '2.0 Flash Thinking': '2.0-flash-thinking',
-      '2.5 Pro': '2.5-pro'
+      '2.0-flash': '2.0 Flash',
+      '2.0-flash-thinking': '2.0 Flash Thinking',
+      '2.5-pro': '2.5 Pro'
     }
   },
   ChatGPT: {
@@ -38,20 +42,21 @@ export const CHATBOTS: Chatbot = {
   'GitHub Copilot': {
     url: 'https://github.com/copilot',
     models: {
-      'GPT-4o': '4o',
+      '4o': 'GPT-4o',
       o1: 'o1',
       'o3-mini': 'o3-mini',
-      'Claude 3.5 Sonnet': 'sonnet-3.5',
-      'Claude 3.7 Sonnet': 'sonnet-3.7',
-      'Claude 3.7 Sonnet Thinking': 'sonnet-3.7-thinking',
-      'Gemini 2.0 Flash': 'gemini-2.0-flash'
+      'sonnet-3.5': 'Claude 3.5 Sonnet',
+      'sonnet-3.7': 'Claude 3.7 Sonnet',
+      'sonnet-3.7-thinking': 'Claude 3.7 Sonnet Thinking',
+      'gemini-2.0-flash': 'Gemini 2.0 Flash'
     }
   },
   Claude: {
     url: 'https://claude.ai/new'
   },
   DeepSeek: {
-    url: 'https://chat.deepseek.com/'
+    url: 'https://chat.deepseek.com/',
+    supported_options: { 'deep-think': 'DeepThink (R1)', search: 'Search' }
   },
   Mistral: {
     url: 'https://chat.mistral.ai/chat'
