1d323a23e9790e99901c5595b662ce5caf02aef8
Robert Piosik <robertpiosik@gmail.com>
2025-04-26 17:31:48 +0200

    Update the homepage heading to display "Free AI coding"

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -14,7 +14,7 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          Free AI coding
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
