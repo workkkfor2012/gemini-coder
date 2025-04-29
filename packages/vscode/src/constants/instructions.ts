fd0b3445956a66e45debf3e42ffc732848e42878
Robert Piosik <robertpiosik@gmail.com>
2025-04-29 02:30:32 +0200

    Update the external code completion instruction to specify response format and add an alias for replacement

--- packages/vscode/src/constants/instructions.ts
+++ packages/vscode/src/constants/instructions.ts
@@ -1,7 +1,7 @@
 export const code_completion_instruction =
   'Find correct replacement for <missing text> symbol. Send replacement text in "replacement" XML tags.'
 export const code_completion_instruction_external =
-  'Find correct replacement for <missing text> symbol. Send replacement text and explain your reasoning. Always refer to symbol "<missing_text>" as "cursor position".'
+  'Find correct replacement for the <missing text> symbol. Send replacement text in a markdown code block starting with comment "// Code completion" and explain your reasoning. Always refer to symbol "<missing_text>" as "cursor position" and "replacement" as "completion".'
 export const get_refactoring_instruction = (file_path: string) =>
   `User requested refactor of a file${
     file_path ? ` ${file_path}` : ''
