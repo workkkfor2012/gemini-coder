6e71efd84ff03850e2c043c6604aa1bc1375e67b
Robert Piosik <robertpiosik@gmail.com>
2025-05-23 23:19:18 +0200

    Update the method to enable the 'deep-thinking' option in the Doubao chatbot

--- packages/browser/src/content-scripts/send-prompt-content-script/chatbots/doubao.ts
+++ packages/browser/src/content-scripts/send-prompt-content-script/chatbots/doubao.ts
@@ -27,15 +27,33 @@ export const doubao: Chatbot = {
     })
   },
   set_options: async (options: string[]) => {
+    const deep_thinking_button = document.querySelector(
+      'div[data-testid="chat_input"] input[data-testid="upload-file-input"] + button'
+    ) as HTMLButtonElement
+    const mouse_up = new MouseEvent('mouseup', { bubbles: true })
+    deep_thinking_button.dispatchEvent(mouse_up)
+    await new Promise((r) => requestAnimationFrame(r))
+    const portal = document.querySelector('.semi-portal')
+    const menu_items = portal!.querySelectorAll('li[role="menuitem"]')
+    const last_menu_item = menu_items[menu_items.length - 1] as HTMLElement
+    const mouse_down = new MouseEvent('mousedown', { bubbles: true })
+    last_menu_item.dispatchEvent(mouse_down)
+    await new Promise((r) => requestAnimationFrame(r))
     const supported_options = CHATBOTS['Doubao'].supported_options
     for (const option of options) {
       if (option == 'deep-thinking' && supported_options['deep-thinking']) {
         const deep_thinking_button = document.querySelector(
-          'div[data-testid="use-deep-thinking-switch-btn"] > button'
+          'div[data-testid="chat_input"] input[data-testid="upload-file-input"] + button'
         ) as HTMLButtonElement
-        // Their script has such listener
         const mouse_up = new MouseEvent('mouseup', { bubbles: true })
         deep_thinking_button.dispatchEvent(mouse_up)
+        await new Promise((r) => requestAnimationFrame(r))
+        const portal = document.querySelector('.semi-portal')
+        const menu_items = portal!.querySelectorAll('li[role="menuitem"]')
+        const last_menu_item = menu_items[1] as HTMLElement
+        const mouse_down = new MouseEvent('mousedown', { bubbles: true })
+        last_menu_item.dispatchEvent(mouse_down)
+        await new Promise((r) => requestAnimationFrame(r))
       }
     }
   },
