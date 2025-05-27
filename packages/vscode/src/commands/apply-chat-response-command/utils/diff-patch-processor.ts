c11554a87fdb1505f3d91a25bcf0a9168bea091a
djex <959760+djex@users.noreply.github.com>
2025-05-26 20:52:18 -0400

    Fixed two issues causing patch apply to fail in certain scenarios (#279)
    * added debug function and fixed issue with trailing empty new line in last search block.
    
    * Refactor the diff patch processor to use structured logging and throw errors
    
    * fixed two issues causing patch apply to fail

--- packages/vscode/src/commands/apply-chat-response-command/utils/diff-patch-processor.ts
+++ packages/vscode/src/commands/apply-chat-response-command/utils/diff-patch-processor.ts
@@ -312,12 +312,17 @@ const apply_diff_patch = (
         replace_chunks.pop()
       }
 
-      // Add the final search block to the searchReplaceBlocks
-      // This is crucial for diffs that only have deletions
-      search_replace_blocks.push(
-        new SearchBlock(search_chunks, replace_chunks, -1)
-      )
+      if (search_chunks.length != 0 || replace_chunks.length != 0) // Don't add search or replace blocks if empty
+      {
+        // Add the final search block to the searchReplaceBlocks
+        // This is crucial for diffs that only have deletions
+        search_replace_blocks.push(
+          new SearchBlock(search_chunks, replace_chunks, -1)
+        )
+      }
     }
+
+    //print_debug(original_code_lines, original_code_lines_normalized, patch_lines_normalized, search_replace_blocks);
 
     // === Work through search and replace blocks finding start of search block ===
     let previous_found_index = 0
@@ -346,28 +351,53 @@ const apply_diff_patch = (
 
         // console.log('Chunk string: ' + chunk_string);
 
-        // Check if the chunk matches the search string
-        if (chunk_string == search_string) {
-          // Check if found index is greater than the previous found index
-          if (previous_found_index >= chunk[0].key) {
-            // This should never happen
-            throw new Error('Found index is less than previous found index')
-          }
-
-          // Store the index of the first line of the search block
-          search_replace_block.search_block_start_index = chunk[0].key
+        // If we have no search lines and only replace lines and our previous found index is 0
+        // assume we are at the start of the file and set the search block start index to 0
+        // This is a special case when a patch hunk starts with one or more + lines and no context lines
+        if (search_replace_block.search_lines.length == 0 && previous_found_index == 0) 
+        {
+          search_replace_block.search_block_start_index = -1 // insert at the start of the file
           found = true
+        }
+        else
+        {
+          // Create a chunk of lines from originalCodeLinesNormalized
+          const chunk = original_code_lines_normalized.slice(
+            j,
+            j + search_replace_block.search_lines.length
+          )
 
-          previous_found_index = chunk[0].key // Update the previous index
+          const chunk_string = chunk.map((line) => line.value).join('')
 
-          //console.log('Found search block at line ' + chunk[0].key);
-          break
+          //console.log('Chunk string: ' + chunk_string);
+          //console.log('Previous found index: ' + previous_found_index);
+          //console.log('Chunk: ' + JSON.stringify(chunk, null, 2));
+       
+          // Check if the chunk matches the search string
+          if (chunk_string == search_string) {
+            // Check if found index is greater than the previous found index
+            if (previous_found_index > chunk[0].key) {
+              // This should never happen
+              throw new Error('Found index is less than previous found index')
+            }
+
+            // Store the index of the first line of the search block
+            search_replace_block.search_block_start_index = chunk[0].key
+            found = true
+
+            previous_found_index = chunk[0].key // Update the previous index
+
+            //console.log('Found search block at line ' + chunk[0].key);
+            break
+          }
         }
       }
 
-      // If not found, set the search_block_start_index to -1
+      // If not found, set the search_block_start_index to -2
       if (!found) {
-        const error_message = `Search block not found: ${search_string}`
+        search_replace_block.search_block_start_index = -2 // Not found
+
+         const error_message = `Search block not found: ${search_string}`
         Logger.error({
           function_name: 'apply_diff_patch',
           message: error_message
@@ -386,7 +416,7 @@ const apply_diff_patch = (
     // === Apply the search and replace blocks ===
     // Get all blocks with a valid start index
     const valid_blocks = search_replace_blocks.filter(
-      (block) => block.get_start_index() !== -1
+      (block) => block.get_start_index() !== -2
     )
 
     // Sort blocks by descending index
@@ -396,7 +426,7 @@ const apply_diff_patch = (
 
     // Iterrate over the valid blocks and apply them
     for (const block of valid_blocks) {
-      const start_index = block.get_start_index()
+      const start_index = block.get_start_index() == -1 ? 0 : block.get_start_index() // When -1, insert at the start of the file
       const search_count = block.get_search_count()
       const replacement_content = block.replace_lines // These are original lines
 
