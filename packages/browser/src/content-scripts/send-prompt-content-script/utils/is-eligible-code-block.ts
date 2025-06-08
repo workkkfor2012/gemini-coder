9ebb061bdfc6e0065a86b834e59ed544672245b6
Robert Piosik <robertpiosik@gmail.com>
2025-06-09 00:27:49 +0200

    Remove '+++' as an eligibility criterion for code blocks

--- packages/browser/src/content-scripts/send-prompt-content-script/utils/is-eligible-code-block.ts
+++ packages/browser/src/content-scripts/send-prompt-content-script/utils/is-eligible-code-block.ts
@@ -1,12 +1,9 @@
 import { extract_path_from_line_of_code } from '@shared/utils/extract-path-from-line-of-code'
 
-export function is_eligible_code_block(
-  first_line_text: string
-): boolean {
+export function is_eligible_code_block(first_line_text: string): boolean {
   return (
     !!first_line_text &&
     (first_line_text.startsWith('---') ||
-      first_line_text.startsWith('+++') ||
       first_line_text.startsWith('diff --git') ||
       !!extract_path_from_line_of_code(first_line_text))
   )
