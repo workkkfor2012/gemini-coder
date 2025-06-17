7e2549c71f87838913b2502764f30f224574367a
Robert Piosik <robertpiosik@gmail.com>
2025-06-18 01:24:47 +0200

    Correct "home tab" to "Home tab" for consistent capitalization

--- packages/vscode/src/view/frontend/tabs/settings/Settings.tsx
+++ packages/vscode/src/view/frontend/tabs/settings/Settings.tsx
@@ -69,7 +69,7 @@ export const Settings: React.FC<Props> = (props) => {
           Configure API Providers
         </UiButton>
         <span>
-          A provider is needed to setup API tools used on home tab and via the
+          A provider is needed to setup API tools used on Home tab and via the
           command palette. API keys are&nbsp;
           <a href="https://code.visualstudio.com/api/references/vscode-api#SecretStorage">
             stored encrypted
