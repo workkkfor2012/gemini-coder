161df0770d809ba0a883eadf236cee8e4e06b95d
Robert Piosik <robertpiosik@gmail.com>
2025-06-20 12:06:51 +0200

    Integrate SimpleBar to provide custom scrolling for the HomeView component

--- packages/vscode/src/view/frontend/tabs/home/HomeView/HomeView.tsx
+++ packages/vscode/src/view/frontend/tabs/home/HomeView/HomeView.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import styles from './HomeView.module.scss'
+import SimpleBar from 'simplebar-react'
 import { Presets as UiPresets } from '@ui/components/editor/Presets'
 import { ChatInput as UiChatInput } from '@ui/components/editor/ChatInput'
 import { Separator as UiSeparator } from '@ui/components/editor/Separator'
@@ -61,7 +62,8 @@ export const HomeView: React.FC<Props> = (props) => {
   const is_in_code_completions_mode =
     (props.home_view_type == 'Web chats' &&
       props.web_mode == 'code-completions') ||
-    (props.home_view_type == 'API' && props.api_mode == 'code-completions')
+    (props.home_view_type == 'API calls' &&
+      props.api_mode == 'code-completions')
 
   const current_prompt = is_in_code_completions_mode
     ? props.code_completion_suggestions
@@ -150,212 +152,223 @@ export const HomeView: React.FC<Props> = (props) => {
   }
 
   return (
-    <div
-      className={styles.container}
-      style={{ display: !props.is_visible ? 'none' : undefined }}
-    >
-      <div className={styles.top}>
-        <UiSwitch
-          value={props.home_view_type}
-          on_change={props.on_home_view_type_change}
-          options={Object.values(HOME_VIEW_TYPES)}
-          title="Initialize web chats or update files right away"
-        />
-
-        <div className={styles.top__dropdown}>
-          {props.home_view_type == HOME_VIEW_TYPES.WEB && (
-            <UiDropdown
-              options={[
-                { value: 'ask', label: 'Ask about context' },
-                { value: 'edit', label: 'Edit context' },
-                {
-                  value: 'code-completions',
-                  label: 'Code at cursor with context'
-                }
-              ]}
-              selected_value={props.web_mode}
-              on_change={props.on_web_mode_change}
-              title="Select mode"
+    <div className={styles.container}>
+      <SimpleBar
+        style={{
+          height: '100%',
+          display: !props.is_visible ? 'none' : undefined
+        }}
+      >
+        <div className={styles.inner}>
+          <div className={styles.top}>
+            <UiSwitch
+              value={props.home_view_type}
+              on_change={props.on_home_view_type_change}
+              options={Object.values(HOME_VIEW_TYPES)}
+              title="Initialize web chats or update files right away"
             />
-          )}
-          {props.home_view_type == HOME_VIEW_TYPES.API && (
-            <UiDropdown
-              options={[
-                { value: 'edit', label: 'Edit context' },
-                {
-                  value: 'code-completions',
-                  label: 'Code at cursor with context'
-                }
-              ]}
-              selected_value={props.api_mode}
-              on_change={props.on_api_mode_change}
-              title="Select mode"
-            />
-          )}
-        </div>
-      </div>
 
-      <UiSeparator height={8} />
-
-      {!props.is_connected && props.home_view_type == HOME_VIEW_TYPES.WEB && (
-        <>
-          <div className={styles['browser-extension-message']}>
-            <span>
-              Get the Connector browser extension for hands-free chat
-              inititalizations
-            </span>
-            <a href="https://chromewebstore.google.com/detail/code-web-chat-connector/ljookipcanaglfaocjbgdicfbdhhjffp">
-              Chrome Web Store ↗
-            </a>
-            <a href="https://addons.mozilla.org/en-US/firefox/addon/gemini-coder-connector/">
-              Firefox Add-ons ↗
-            </a>
+            <div className={styles.top__dropdown}>
+              {props.home_view_type == HOME_VIEW_TYPES.WEB && (
+                <UiDropdown
+                  options={[
+                    { value: 'ask', label: 'Ask about context' },
+                    { value: 'edit', label: 'Edit context' },
+                    {
+                      value: 'code-completions',
+                      label: 'Code at cursor with context'
+                    }
+                  ]}
+                  selected_value={props.web_mode}
+                  on_change={props.on_web_mode_change}
+                  title="Select mode"
+                />
+              )}
+              {props.home_view_type == HOME_VIEW_TYPES.API && (
+                <UiDropdown
+                  options={[
+                    { value: 'edit', label: 'Edit context' },
+                    {
+                      value: 'code-completions',
+                      label: 'Code at cursor with context'
+                    }
+                  ]}
+                  selected_value={props.api_mode}
+                  on_change={props.on_api_mode_change}
+                  title="Select mode"
+                />
+              )}
+            </div>
           </div>
 
           <UiSeparator height={8} />
-        </>
-      )}
 
-      <div className={styles['chat-input']}>
-        <UiChatInput
-          value={current_prompt}
-          chat_history={props.chat_history}
-          chat_history_fim_mode={props.chat_history_fim_mode}
-          on_change={handle_input_change}
-          on_submit={handle_submit}
-          on_submit_with_control={handle_submit_with_control}
-          on_copy={
-            props.home_view_type == HOME_VIEW_TYPES.WEB
-              ? handle_copy
-              : undefined
-          }
-          on_at_sign_click={props.on_at_sign_click}
-          is_web_mode={props.home_view_type == HOME_VIEW_TYPES.WEB}
-          is_connected={props.is_connected}
-          token_count={estimated_input_tokens}
-          submit_disabled_title={
-            !props.is_connected
-              ? 'WebSocket connection not established. Please install the browser extension.'
-              : 'Type something'
-          }
-          is_in_code_completions_mode={is_in_code_completions_mode}
-          has_active_selection={props.has_active_selection}
-          has_active_editor={props.has_active_editor}
-          on_caret_position_change={props.on_caret_position_change}
-          translations={{
-            type_something: 'Type something',
-            completion_suggestions: 'Completion suggestions',
-            send_request: 'Send request',
-            initialize_chat: 'Initialize chat',
-            select_preset: 'Select preset',
-            select_config: 'Select config',
-            code_completions_mode_unavailable_with_text_selection:
-              'Unable to work with text selection',
-            code_completions_mode_unavailable_without_active_editor:
-              'This mode requires active editor'
-          }}
-        />
-      </div>
+          {!props.is_connected &&
+            props.home_view_type == HOME_VIEW_TYPES.WEB && (
+              <>
+                <div className={styles['browser-extension-message']}>
+                  <span>
+                    Get the Connector browser extension for hands-free chat
+                    inititalizations
+                  </span>
+                  <a href="https://chromewebstore.google.com/detail/code-web-chat-connector/ljookipcanaglfaocjbgdicfbdhhjffp">
+                    Chrome Web Store ↗
+                  </a>
+                  <a href="https://addons.mozilla.org/en-US/firefox/addon/gemini-coder-connector/">
+                    Firefox Add-ons ↗
+                  </a>
+                </div>
 
-      {props.home_view_type == HOME_VIEW_TYPES.WEB &&
-        props.web_mode == 'edit' &&
-        props.edit_format_selector_visibility == 'visible' && (
-          <>
-            <UiSeparator height={10} />
-            <div className={styles['edit-format']}>
-              <span>RESPONSE EDIT FORMAT</span>
-              <UiHorizontalSelector
-                options={[
-                  {
-                    value: 'whole',
-                    label: 'Whole',
-                    title: 'The model will output complete files'
-                  },
-                  {
-                    value: 'truncated',
-                    label: 'Truncated',
-                    title: 'The model will skip unchanged fragments'
-                  },
-                  {
-                    value: 'diff',
-                    label: 'Diff',
-                    title: 'The model will output diffs'
+                <UiSeparator height={8} />
+              </>
+            )}
+
+          <div className={styles['chat-input']}>
+            <UiChatInput
+              value={current_prompt}
+              chat_history={props.chat_history}
+              chat_history_fim_mode={props.chat_history_fim_mode}
+              on_change={handle_input_change}
+              on_submit={handle_submit}
+              on_submit_with_control={handle_submit_with_control}
+              on_copy={
+                props.home_view_type == HOME_VIEW_TYPES.WEB
+                  ? handle_copy
+                  : undefined
+              }
+              on_at_sign_click={props.on_at_sign_click}
+              is_web_mode={props.home_view_type == HOME_VIEW_TYPES.WEB}
+              is_connected={props.is_connected}
+              token_count={estimated_input_tokens}
+              submit_disabled_title={
+                !props.is_connected
+                  ? 'WebSocket connection not established. Please install the browser extension.'
+                  : 'Type something'
+              }
+              is_in_code_completions_mode={is_in_code_completions_mode}
+              has_active_selection={props.has_active_selection}
+              has_active_editor={props.has_active_editor}
+              on_caret_position_change={props.on_caret_position_change}
+              translations={{
+                type_something: 'Type something',
+                completion_suggestions: 'Completion suggestions',
+                send_request: 'Send request',
+                initialize_chat: 'Initialize chat',
+                select_preset: 'Select preset',
+                select_config: 'Select config',
+                code_completions_mode_unavailable_with_text_selection:
+                  'Unable to work with text selection',
+                code_completions_mode_unavailable_without_active_editor:
+                  'This mode requires active editor'
+              }}
+            />
+          </div>
+
+          {props.home_view_type == HOME_VIEW_TYPES.WEB &&
+            props.web_mode == 'edit' &&
+            props.edit_format_selector_visibility == 'visible' && (
+              <>
+                <UiSeparator height={10} />
+                <div className={styles['edit-format']}>
+                  <span>RESPONSE EDIT FORMAT</span>
+                  <UiHorizontalSelector
+                    options={[
+                      {
+                        value: 'whole',
+                        label: 'Whole',
+                        title: 'The model will output complete files'
+                      },
+                      {
+                        value: 'truncated',
+                        label: 'Truncated',
+                        title: 'The model will skip unchanged fragments'
+                      },
+                      {
+                        value: 'diff',
+                        label: 'Diff',
+                        title: 'The model will output diffs'
+                      }
+                    ]}
+                    selected_value={
+                      props.web_mode == 'edit' ? props.edit_format : undefined
+                    }
+                    on_select={props.on_edit_format_change}
+                  />
+                </div>
+              </>
+            )}
+
+          <UiSeparator height={16} />
+          <div className={styles['quick-actions']}>
+            <span>SUGGESTED COMMANDS</span>
+            {props.home_view_type == HOME_VIEW_TYPES.WEB &&
+              (props.web_mode == 'edit' ||
+                props.web_mode == 'code-completions') && (
+                <UiQuickAction
+                  title="Apply Chat Response"
+                  description="Integrate copied message or a code block"
+                  on_click={() =>
+                    props.on_quick_action_click('codeWebChat.applyChatResponse')
                   }
-                ]}
-                selected_value={
-                  props.web_mode == 'edit' ? props.edit_format : undefined
+                />
+              )}
+            {((props.home_view_type == HOME_VIEW_TYPES.WEB &&
+              props.web_mode != 'ask') ||
+              (props.home_view_type == HOME_VIEW_TYPES.API &&
+                props.api_mode != 'code-completions')) && (
+              <UiQuickAction
+                title="Revert Last Changes"
+                description="Restore saved state of the codebase"
+                on_click={() =>
+                  props.on_quick_action_click('codeWebChat.revert')
                 }
-                on_select={props.on_edit_format_change}
               />
-            </div>
-          </>
-        )}
-
-      <UiSeparator height={16} />
-      <div className={styles['quick-actions']}>
-        <span>SUGGESTED COMMANDS</span>
-        {props.home_view_type == HOME_VIEW_TYPES.WEB &&
-          (props.web_mode == 'edit' ||
-            props.web_mode == 'code-completions') && (
+            )}
             <UiQuickAction
-              title="Apply Chat Response"
-              description="Integrate copied message or a code block"
+              title="Commit Changes"
+              description="Generate a commit message and commit"
               on_click={() =>
-                props.on_quick_action_click('codeWebChat.applyChatResponse')
+                props.on_quick_action_click('codeWebChat.commitChanges')
               }
             />
-          )}
-        {((props.home_view_type == HOME_VIEW_TYPES.WEB &&
-          props.web_mode != 'ask') ||
-          (props.home_view_type == HOME_VIEW_TYPES.API &&
-            props.api_mode != 'code-completions')) && (
-          <UiQuickAction
-            title="Revert Last Changes"
-            description="Restore saved state of the codebase"
-            on_click={() => props.on_quick_action_click('codeWebChat.revert')}
-          />
-        )}
-        <UiQuickAction
-          title="Commit Changes"
-          description="Generate a commit message and commit"
-          on_click={() =>
-            props.on_quick_action_click('codeWebChat.commitChanges')
-          }
-        />
-      </div>
+          </div>
 
-      {props.home_view_type == HOME_VIEW_TYPES.WEB && (
-        <>
-          <UiSeparator height={16} />
-          <UiPresets
-            presets={props.presets.map((preset) => {
-              return {
-                ...preset,
-                has_affixes: !!(preset.prompt_prefix || preset.prompt_suffix)
-              }
-            })}
-            is_disabled={!props.is_connected}
-            selected_presets={props.selected_presets}
-            selected_code_completion_presets={
-              props.selected_code_completion_presets
-            }
-            on_create_preset={props.on_create_preset}
-            on_preset_click={(name) => {
-              props.initialize_chats({
-                prompt: current_prompt,
-                preset_names: [name]
-              })
-            }}
-            on_preset_copy={handle_preset_copy}
-            on_preset_edit={props.on_preset_edit}
-            is_in_code_completions_mode={is_in_code_completions_mode}
-            on_presets_reorder={props.on_presets_reorder}
-            on_preset_duplicate={props.on_preset_duplicate}
-            on_preset_delete={props.on_preset_delete}
-            on_set_default_presets={props.on_set_default_presets}
-          />
-        </>
-      )}
+          {props.home_view_type == HOME_VIEW_TYPES.WEB && (
+            <>
+              <UiSeparator height={16} />
+              <UiPresets
+                presets={props.presets.map((preset) => {
+                  return {
+                    ...preset,
+                    has_affixes: !!(
+                      preset.prompt_prefix || preset.prompt_suffix
+                    )
+                  }
+                })}
+                is_disabled={!props.is_connected}
+                selected_presets={props.selected_presets}
+                selected_code_completion_presets={
+                  props.selected_code_completion_presets
+                }
+                on_create_preset={props.on_create_preset}
+                on_preset_click={(name) => {
+                  props.initialize_chats({
+                    prompt: current_prompt,
+                    preset_names: [name]
+                  })
+                }}
+                on_preset_copy={handle_preset_copy}
+                on_preset_edit={props.on_preset_edit}
+                is_in_code_completions_mode={is_in_code_completions_mode}
+                on_presets_reorder={props.on_presets_reorder}
+                on_preset_duplicate={props.on_preset_duplicate}
+                on_preset_delete={props.on_preset_delete}
+                on_set_default_presets={props.on_set_default_presets}
+              />
+            </>
+          )}
+        </div>
+      </SimpleBar>
     </div>
   )
 }
